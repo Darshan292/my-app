fcfd3ec623303505a00762e9828922b96bcaa6ed
Darshan U Shetty <96349479+Darshan292@users.noreply.github.com>
2026-03-13 04:48:37 +0000

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
 import { defineConfig } from "vite";
-import { miaodaDevPlugin } from "miaoda-sc-plugin";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    miaodaDevPlugin(),
     svgr({
       svgrOptions: {
         icon: true,
@@ -17,6 +14,13 @@ export default defineConfig({
       },
     }),
   ],
+
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    strictPort: true
+  },
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
